5e4e333ebbfbd48a4e7f8e0ff5fc83c5eb13f561
midzer <midzer@gmail.com>
2022-11-23 14:52:21 +0100

    new favicon, service worker and dir structure

--- assets/js/app.js
+++ assets/js/app.js
@@ -177,3 +177,23 @@ window.addEventListener('DOMContentLoaded', function() {
 	    }
     });
 });
+// Install Service Worker
+if ('serviceWorker' in navigator) {
+  // Delay registration until after the page has loaded, to ensure that our
+  // precaching requests don't degrade the first visit experience.
+  // See https://developers.google.com/web/fundamentals/instant-and-offline/service-worker/registration
+  window.addEventListener('load', function () {
+    // Your service-worker.js *must* be located at the top-level directory relative to your site.
+    // It won't be able to control pages unless it's located at the same level or higher than them.
+    // *Don't* register service worker file in, e.g., a scripts/ sub-directory!
+    // See https://github.com/slightlyoff/ServiceWorker/issues/468
+    
+    navigator.serviceWorker.register('/sw.js')
+    
+    navigator.serviceWorker.ready.then(function() {
+      console.log('Service worker registered')
+    }).catch(function (e) {
+      console.error('Error during service worker registration, possibly cookies are blocked:', e)
+    })
+  })
+}
---
+++ sw.js
@@ -0,0 +1,35 @@
+const version = '1.0.0';
+const cacheName = `filelove-${version}`;
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(
+        [
+          '/index.html',
+          '/assets/css/style.css',
+          '/assets/js/webtorrent.min.js',
+          '/assets/js/prettier-bytes.min.js',
+          '/assets/js/upload-element.min.js',
+          '/assets/js/app.js'
+        ]
+      );
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
